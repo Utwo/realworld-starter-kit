c078060355b6e40341070798d1876c0375c49406
jaonoctus <joaodias@noctus.org>
2020-01-23 16:32:22 -0300

    :truck: Rename test

--- test/functional/login.spec.js
+++ test/functional/login.spec.js
@@ -5,7 +5,7 @@ const Factory = use('Factory')
 
 trait('Test/ApiClient')
 
-test('login user', async ({ client }) => {
+test('Should return the user data with valid credentials', async ({ client }) => {
   const user = await Factory.model('App/Models/User').create()
 
   const data = {
